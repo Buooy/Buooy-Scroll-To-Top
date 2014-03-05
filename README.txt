0a9876c064dafc5b0baf8e70874bfca49f46e2d9
Buooy <aaron.lee@buooy.com>
2014-03-06 01:21:50 +0800

    First Commit

---
+++ README.txt
@@ -0,0 +1,40 @@
+=== Buooy Scroll To Top ===
+Contributors: Buooy
+Requires at least: 3.8.1
+Tested up to: 3.8.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Buooy Scroll To Top is a Scroll to Top that actually looks nice. And its incredibly easy to use. Just activate and go!
+
+== Description ==
+
+Buooy Scroll To Top is a Scroll to Top that actually looks nice. And its incredibly easy to use. Just activate and go!
+
+== Installation ==
+
+= Uploading in WordPress Dashboard =
+
+1. Navigate to the 'Add New' in the plugins dashboard
+2. Navigate to the 'Upload' area
+3. Select `buooy-scrolltotop.zip` from your computer
+4. Click 'Install Now'
+5. Activate the plugin in the Plugin dashboard
+
+= Using FTP =
+
+1. Download `buooy-scrolltotop.zip`
+2. Extract the `buooy-scrolltotop` directory to your computer
+3. Upload the `buooy-scrolltotop` directory to the `/wp-content/plugins/` directory
+4. Activate the plugin in the Plugin dashboard
+
+
+== Frequently Asked Questions ==
+
+= I would post a question, but no one has asked! =
+
+== Changelog ==
+
+= 1.0 =
+* First Upload
---
+++ buooy-scrolltotop.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Buooy Scroll To Top
+ *
+ * Buooy Scroll To Top is a Scroll to Top that actually looks nice. And its incredibly easy to use. Just activate and go!
+ *
+ * @package   Buooy_Scrolltotop
+ * @author    Aaron Lee <aaron.lee@buooy.com>
+ * @license   GPL-2.0+
+ * @link      http://buooy
+ * @copyright 2014 Buooy
+ *
+ * @buooy_scrolltotop
+ * Plugin Name:       Buooy Scroll To Top
+ * Plugin URI:        http://buooy.com
+ * Description:       Buooy Scroll To Top is a Scroll to Top that actually looks nice. And its incredibly easy to use. Just activate and go!
+ * Version:           1.0.0
+ * Author:            Aaron Lee
+ * Author URI:        http://buooy.com/
+ * Text Domain:       buooy-scrolltotop-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+ 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+Class Buooy_Scrolltotop{
+
+	protected $prefix 		= 	"stt_";
+	protected $offset_top	=	"200";
+	protected $plugin_url;
+
+	// Initialise the scrolltotop
+    public function __construct() {
+    	$this->plugin_url = plugins_url( "", __FILE__ );
+
+    	add_action( 'wp_head', array( $this,'wp_head_style') );
+    	add_action( 'wp_footer', array( $this, 'wp_head_script') );
+        add_action( 'wp_head', array( $this,'wp_head_element') );
+    }
+
+    // Add Element
+    public function wp_head_element(){
+    	$element 	= 	"<div class='".$this->prefix."container'>";
+    	$element 	.= 	"<img class='".$this->prefix."image' src='".$this->plugin_url."/backtotop.png'>";
+    	$element 	.= 	"</div>";
+
+    	echo $element;
+    }
+
+	// Add Script
+	public function wp_head_script(){
+		
+		$script 	= "<script>";
+		$script 	.= "
+						jQuery(document).scroll(function(){
+							if( jQuery(document).scrollTop() > ".$this->offset_top." ){
+								jQuery('.".$this->prefix."container').fadeIn();
+							}
+							else{
+								jQuery('.".$this->prefix."container').fadeOut();
+							}
+						});
+						jQuery('.".$this->prefix."container').click(function(){
+							jQuery('html,body').animate({
+								scrollTop: '0px'
+							},500);
+						});
+						";
+		$script 	.= "</script>";
+
+		echo $script;
+
+	}
+
+	// Add Style
+	public function wp_head_style(){
+		$style 	= 	"<style>";
+		$style 	.=	"
+						.".$this->prefix."container{
+							display:	none;
+							opacity: 	0.75;
+							position: 	fixed;
+							height:		48px;
+							bottom:		15px;
+							right:		15px;
+						}
+						.".$this->prefix."container:hover{
+							opacity: 	1;
+							cursor:		pointer;
+						}	
+						.".$this->prefix."image{
+							height:		48px;
+						}
+					";	
+		$style 	.=	"</style>";
+		
+		echo $style;
+	}
+
+}
+
+$buooy_scrolltotop = new Buooy_Scrolltotop;
